7ee034c6ad769e024fa3f37c19ca2a84308351f2
abigailBC <mixolida36@gmail.com>
2025-06-05 13:43:02 +0200

    pantalla showMyAnimal

--- app/src/main/java/com/example/animor/UI/ViewsFrames/CreateAnimalFragment.java
+++ app/src/main/java/com/example/animor/UI/ViewsFrames/CreateAnimalFragment.java
@@ -315,7 +315,7 @@ public class CreateAnimalFragment extends Fragment {
         UserDTO user = PreferenceUtils.getUser();
         String fileName = "animal_" + System.currentTimeMillis() + ".jpg";
         StorageReference imageRef = storageReference.child("foto/"+user.getUserId() +"/"+fileName);
-        imagePath = imageRef.getPath().substring(1);
+        imagePath = imageRef.getPath();
 
         // Subir archivo
         imageRef.putFile(uri)
